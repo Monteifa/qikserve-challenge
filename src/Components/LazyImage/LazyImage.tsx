ba9c54525fcaedde84bcbe3bebe4c61d6629243d
Monteifa <fabiopmont@gmail.com>
2024-09-19 17:47:20 -0300

    Improving error handling

--- src/Components/LazyImage/LazyImage.tsx
+++ src/Components/LazyImage/LazyImage.tsx
@@ -1,21 +1,24 @@
+import { useState } from 'react';
 import { useDataContext } from '../../contexts/DataContext';
 
 const LazyImage = ({ src }: { src: string | undefined }) => {
   const { loading } = useDataContext();
+  const [error, setError] = useState(false);
 
   return (
     <div>
       {loading && !src && <p>Loading...</p>}
 
-      {!loading && !src && <p>Error loading image</p>}
+      {!loading && error && <p>Error loading image</p>}
 
       {!loading && src && (
         <img
-          src={src}
+          src={error ? '' : src}
           loading='lazy'
           alt='Image menu item'
           width={128}
           height={85}
+          onError={() => setError(true)}
           className='menu_item_img'
         />
       )}
